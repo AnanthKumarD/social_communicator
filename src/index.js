a24a36e2894176a42c7f6b7840c9b53cbe261af4
andodda <ananthkumard89@gmail.com>
2020-04-24 16:16:15 +0530

    changes in APP.js file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/popper.js/dist/umd/popper.js';
 import '../node_modules/bootstrap/dist/js/bootstrap';
 import App from './App';
-import './sass/main.scss'; 
+import './sass/main.scss';
 //import * as serviceWorker from './serviceWorker';
 import rootReducer from './reducers/rootReducer';
 import { createStore } from 'redux';
@@ -15,7 +15,7 @@ import * as photoAlbumAction from './actions/photoAlbumAction';
 import * as videoAlbumAction from './actions/videoAlbumAction';
 
 
-import { HashRouter, Switch} from 'react-router-dom';
+import { HashRouter, Switch } from 'react-router-dom';
 // import createBrowserHistory from "history/createBrowserHistory";
 
 //const customHistory = createBrowserHistory();
@@ -26,16 +26,19 @@ store.dispatch(friendsActions.getFriends());
 store.dispatch(photoAlbumAction.photoAlbumAction());
 store.dispatch(videoAlbumAction.videoAlbumAction());
 
-ReactDOM.render(
-	<Provider store={store}>
-	 <HashRouter>
-	 	<Switch>
-			<App />
-		</Switch>
-	 </HashRouter>
-	</Provider>	, document.getElementById('root'));
+ReactDOM.render( <
+        Provider store = { store } >
+        <
+        HashRouter >
+        <
+        Switch >
+        <
+        App / >
+        <
+        /Switch> <
+        /HashRouter> <
+        /Provider>	, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-
+        // If you want your app to work offline and load faster, you can change
+        // unregister() to register() below. Note this comes with some pitfalls.
+        // Learn more about service workers: http://bit.ly/CRA-PWA
