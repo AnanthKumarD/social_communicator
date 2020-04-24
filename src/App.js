20e997c818d1886c5e1e82803f676e7f77eeab08
andodda <ananthkumard89@gmail.com>
2020-04-24 16:11:13 +0530

    changes in APP.js file

--- src/App.js
+++ src/App.js
@@ -6,27 +6,36 @@ import './App.scss';
 import Profile from './component/profileComp/ProfileComp';
 import Header from './common/header/Header.js';
 // import LeftSideBarMenu  from './common/leftSideBarMenu/LeftSideBarMenu.js';
-import ChatMenuBar  from './common/chatMenu/ChatMenuBar.js';
+// import ChatMenuBar  from './common/chatMenu/ChatMenuBar.js';
 
 import FriendsGallery from './component/friends/FriendsGallery';
 import PhotosGallery from './component/photosComp/PhotosGallery';
 import VideosGallery from './component/videos/VideosGallery';
-
+// <Header / >
+        // <ChatMenuBar />
 
 const AppRouter = () => ( 
     <Fragment >
 
-        <Header / >
-        <ChatMenuBar />
+        
 
         <Switch>
         <Route exact path="/" render={() => (
-            <Redirect to="/profile"/>
+            <Redirect to="/"/>
         )}/>
 
-        <Route path="/" exact name="Home" component={Profile} />
-        <Route path="/profile"  name="Home1" component={Profile} />
-        <Route path="/friendsList" name="FriendsGallery" component={FriendsGallery} />
+        <Route
+            path="/"
+            exact
+            name="Home"
+            component={Header} 
+        />
+        <Route 
+            path="/profile" 
+            name="Home1"
+            component={Profile} 
+         />
+        <Route path="/friendsList" name="FriendsGallery"component={FriendsGallery} />
         <Route path="/photosList" name="PhotosGallery" component={PhotosGallery} />
         <Route path="/videosList" name="VideosGallery" component={VideosGallery} />
       </Switch>
